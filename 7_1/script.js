9010be6009f0e39ef48371740f8649fd840eabbb
rafMewtwo <ponciano.raf@gmail.com>
2020-08-11 15:20:04 -0300

    lets go pikachu

--- 7_1/script.js
+++ 7_1/script.js
@@ -24,4 +24,68 @@ function editInPlace() {
 }
 editInPlace();
 
+//parte II
+//ex01
+const factorial = number => {
+  let result = 1
 
+  for (let i = 2; i <= number; i += 1) {
+      result *= i
+  }
+
+  return result
+}
+
+//ex02
+const longest = string => {
+  const n = string.split(" ")
+  let result = n[0].length;
+  let maiorPalavra = n[0];
+  for(let i=0;i<n.length;i++){
+    if (n[i].length > result) {
+      maiorPalavra = n[i];
+      result = n[i].length;
+    }
+  }
+  return maiorPalavra;
+}
+console.log(longest("Antônio foi no banheiro e não sabemos o que aconteceu"));
+
+//ex03
+const button = document.getElementById("button");
+let conta = 0;
+button.addEventListener('click', () => {
+  let result = document.getElementById('result')
+  conta++;
+  result.innerHTML = conta;
+})
+
+//ex04
+const array = ["Android", "iOS", "Architecture", "Teach", "Run"]
+
+function buildSkillsPhrase (paramOne) {
+    const fun1 = paramInner => (
+      `Tryber ${paramInner} aqui!
+
+      Tudo bem?`
+    )
+
+    let result = `${fun1(paramOne)}
+
+    Minhas cinco principais habilidades são:`
+
+    array.forEach((skill, index) =>
+    result = `${result}
+
+    - ${skill}`)
+
+    result = `
+    ${result}
+
+    #goTrybe
+    `
+
+    return result
+}
+
+console.log(buildSkillsPhrase("Lucas"))
