e9c5bacfce4e584cd8fcf73f1bd1f12c488a4a2f
rafMewtwo <ponciano.raf@gmail.com>
2020-08-11 01:04:36 -0300

    add ex 01 e 02

---
+++ 7_1/script.js
@@ -0,0 +1,27 @@
+//exe01
+const checkScope = () => {
+    'use strict';
+    let i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+
+console.log(checkScope());
+
+//exe02
+const s = [5, 7, 2];
+function editInPlace() {
+  'use strict';
+  // Only change code below this line
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+  // Only change code above this line
+}
+editInPlace();
+
+
