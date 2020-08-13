69c04e3bcd4b20a0651299d7aab1b2e8ea713ceb
rafMewtwo <ponciano.raf@gmail.com>
2020-08-13 18:04:21 -0300

    add 7_3

---
+++ 7_3/script.js
@@ -0,0 +1,87 @@
+//exe01
+const assert = require('assert');
+
+function sum(a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw new Error('parameters must be numbers');
+  }
+
+  return a + b;
+}
+
+assert.strictEqual(typeof sum, 'function');
+assert.strictEqual(sum(4, 5), 9);
+assert.strictEqual(sum(0, 0), 0);
+assert.throws(() => {
+  sum(4, '5');
+});
+assert.throws(() => {
+  sum(4, '5');
+}, /^Error: parameters must be numbers$/);
+
+//exe02
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+assert.strictEqual(typeof myRemove, 'function');
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemove([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+const myList = [5, 6, 7, 8];
+myRemove(myList, 5);
+assert.deepStrictEqual(myList, [5, 6, 7, 8]);
+
+assert.deepStrictEqual(myRemove([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
+//exe03 
+const assert = require('assert');
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let i = 0, len = arr.length; i < len; i += 1) {
+    if (arr[i] === item) {
+      arr.splice(i, 1);
+      i -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+
+const myList = [1, 2, 3, 4];
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 4]);
+assert.notDeepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 3), [1, 2, 3, 4]);
+
+myRemoveWithoutCopy(myList, 1);
+assert.strictEqual(myList.length, 3);
+
+assert.deepStrictEqual(myRemoveWithoutCopy([1, 2, 3, 4], 5), [1, 2, 3, 4]);
+
+//exe04
+const assert = require('assert');
+
+function myFizzBuzz(num) {
+  if (typeof num !== 'number') return false;
+  if (num % 3 === 0 && num % 5 === 0) return 'fizzbuzz';
+  if (num % 3 === 0) return 'fizz';
+  if (num % 5 === 0) return 'buzz';
+  return num;
+}
+
+// implemente seus testes aqui
+
+assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
+assert.strictEqual(myFizzBuzz(9), 'fizz');
+assert.strictEqual(myFizzBuzz(25), 'buzz');
+assert.strictEqual(myFizzBuzz(17), 17);
+assert.strictEqual(myFizzBuzz('this_is_not_a_number'), false);
